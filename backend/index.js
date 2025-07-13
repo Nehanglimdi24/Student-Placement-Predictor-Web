9a6e454907947419b90e337e3ad8080f64c8016a
Nehanglimdi24 <nehanglgaming@gmail.com>
2025-07-13 10:18:09 +0530

    Trying to configure1

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 
 const app = express();
 app.use(cors({
-  origin: 'https://stdplacementpredictor.netlify.app', // your frontend domain
+  origin: '*', // your frontend domain
   methods: ['GET', 'POST', 'OPTIONS'],
   allowedHeaders: ['Content-Type'],
 }));
