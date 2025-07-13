acea9de7ac09ea00b9336f12dab7981c46aeaeeb
Nehanglimdi24 <nehanglgaming@gmail.com>
2025-07-13 11:27:01 +0530

    Trying to configure 3

--- ml-service/app.py
+++ ml-service/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 CORS(app, resources={r"/predict": {"origins": "*"}})
   # ✅ Enable CORS for all origins
 
-model = joblib.load("./placement_model.pkl")
+model = joblib.load("placement_model.pkl")
 
 @app.route("/predict", methods=["POST"])
 def predict():
